0df3b1bef40c1c56cddef0b8bb1ec86c72b544f9
YashPK <pratapyashkulshrestha@gmail.com>
2024-10-21 22:46:37 +0530

    Add convex library for querying tasks

---
+++ convex/schema.ts
@@ -0,0 +1,9 @@
+import { defineSchema, defineTable } from "convex/server";
+import { authTables } from "@convex-dev/auth/server";
+
+
+const schema = defineSchema({
+    ...authTables
+})
+
+export default schema
